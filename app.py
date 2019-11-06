43a9eb5ffb188716581f2f955a89574b02f94db0
jxh <2072128485@qq.com>
2019-11-06 15:53:19 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask  # 从flask包导入Flask类
 app = Flask(__name__)  # 实例化Flask类
 
@@ -14,7 +15,7 @@ def user_page(name):
     return 'User:%s' % name
 
 
-'''from flask import Flask
+from flask import Flask
 from flask import url_for
 app = Flask(__name__)  # 实例化Flask类
 
@@ -41,3 +42,24 @@ def test_url_for():
     return 'Test page'
 '''
 
+from flask import Flask, render_template
+app = Flask(__name__)
+
+name = "Jay Chou"
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
