f9e5892680bc2a1918e673ffa598486f2020f6ac
jxh <2072128485@qq.com>
2019-11-08 11:23:17 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -43,7 +43,34 @@ def test_url_for():
 '''
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
 
 name = "Jay Chou"
 movies = [
@@ -60,6 +87,24 @@ movies = [
 ]
 
 
+user = User(name=name)
+db.session.add(user)
+for m in movies:
+    movie = Movie(title=m['title'], year=m['year'])
+    db.session.add(movie)
+
+db.session.commit()
+click.echo('Done.')
+
+'''
 @app.route('/')
 def index():
     return render_template('index.html', name=name, movies=movies)
+'''
+
+
+@app.route('/')
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
